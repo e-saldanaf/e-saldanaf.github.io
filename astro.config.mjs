2c18c44b3bfd10c97fefa9041f637d173d7e766b
esaldana <85488195+e-saldanaf@users.noreply.github.com>
2026-05-18 11:46:00 +0200

    feat: initial portfolio setup with Astro Micro

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// astro.config.mjs
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+
+export default defineConfig({
+  site: "https://esaldana.github.io",
+  integrations: [mdx(), sitemap()],
+})
